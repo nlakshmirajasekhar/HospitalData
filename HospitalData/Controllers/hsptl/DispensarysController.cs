eb3a5b689ad17106793f3297b6fd2d088fc068c9
nlakshmirajasekhar <nlakshmirajasekhar@gmail.com>
2021-04-01 17:28:29 +0530

    dispensary

--- HospitalData/Controllers/hsptl/DispensarysController.cs
+++ HospitalData/Controllers/hsptl/DispensarysController.cs
@@ -8,16 +8,87 @@ using HospitalData.Models;
 
 namespace HospitalData.Controllers.hsptl
 {
+
+    public class DispensaryWrapper
+    {
+        public Dispensary dis { get; set; }
+        public int trans { get; set; }
+        public String result { get; set; }
+    
+    
+    
+    }
+
     public class DispensarysController : Controller
     {
         [HttpGet]
-        [Route("api/Labc/getd")]
-        public IQueryable<Dispensary> getd(String? str)
+        [Route("api/Dispensarys/getd")]
+        public IQueryable<Dispensary> getd(int str)
         {
             hospitalsContext db = new hospitalsContext();
-            return db.Dispensary.Where(a=>a.Billno.c
+            return db.Dispensary.Where(a =>a.Billno==str);
         }
+        [HttpPost]
+        [Route("api/Dispensarys/postd")]
+        public DispensaryWrapper postd([FromBody] DispensaryWrapper di)
 
+        {
+            String msg = "";
+            hospitalsContext db = new hospitalsContext();
+            try
+            {
+                switch(di.trans)
+                {
+                    case 1:
+                        db.Dispensary.Add(di.dis);
+                        db.SaveChanges();
+                        msg = "ok";
+                        break;
+                    case 2:
+                        var du = db.Dispensary.Where(a => a.Billno == di.dis.Billno).FirstOrDefault();
+                        du.BillAmt = di.dis.BillAmt;
+                        du.Dat = di.dis.Dat;
+                        du.BillAmt = di.dis.BillAmt;
+                        db.SaveChanges();
+                        msg = "ok";
+                        break;
+                    case 3:
+                        if(Diss(di.dis.AdminssionId))
+                        {
+                            var dd = db.Dispensary.Where(a => a.Billno == di.dis.Billno).FirstOrDefault();
+                            db.Dispensary.Remove(dd);
+                            db.SaveChanges();
+                            msg = "ok";
+                        }
+                        else
+                        {
+                            msg = "not exists";
+                        }
+                        break;
+            }
+            }
+            catch(Exception ee)
+            {
+                msg = ee.Message;
+            }
+            di.result = msg;
+            return di;
+
+        }
+        private Boolean Diss(int? aid)
+        {
+            hospitalsContext db = new hospitalsContext();
+            var x = db.PatientAdmissions.Where(a => a.AdminssionId == aid).FirstOrDefault();
+            if(x==null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
 
     }
 }
